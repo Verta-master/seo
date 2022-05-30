2694a1e04a63c6f151b0fe15ced40daf6c53316f
Verta-master <vertyagina@gmail.com>
2022-05-30 15:04:29 +0600

    mobile

--- js/script.js
+++ js/script.js
@@ -11,17 +11,10 @@ $('.menu__btn').click(function() {
   });
 })
 
-//$('.menu__btn').click(function() {
-//  $('.menu').toggleClass('menu--opened');
-//  $('.menu__list').slideToggle();
-//})
-//
-//if (window.innerWidth < 766) {
-//  $('.menu__link').click(function() {
-//    $('.menu__list').slideToggle();
-//    $('.menu').removeClass('menu--opened');
-//  })
-//}
+$('.menu-btn').click(function() {
+  $('.menu').toggleClass('menu--open');
+  $(this).toggleClass('menu-btn--close');
+})
 
 //Scroll to top
 $(window).scroll(function(){
